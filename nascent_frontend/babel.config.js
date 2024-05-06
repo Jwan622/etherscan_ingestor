c1a13035c36858a9da0b3435fd9a92ea7c62239e
Jeffrey <jwan622@gmail.com>
2024-05-06 16:34:45 -0400

    adding babel so typescript compiles

--- nascent_frontend/babel.config.js
+++ nascent_frontend/babel.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   presets: [
-    ['@babel/preset-env', {
+    ['@babel/preset-flow', {
       targets: {
         node: 'current', // Target the current version of Node
       }
