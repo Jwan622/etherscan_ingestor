e4050ceae9eafe78467143197b76b316117c9bff
Jeffrey <jwan622@gmail.com>
2024-05-06 12:39:33 -0400

    owe you an integration test

---
+++ nascent_etherscan_ingestor/tests/integration/test_integration.py
@@ -0,0 +1,11 @@
+import pytest
+
+
+class TestIngestor:
+    @pytest.mark.skip(reason="I owe you an integration test")
+    def test_multi_threads_ingest(self):
+        """
+        - test from the typer command and inspect the database, actually not too hard and similar to the last 2 unit tests I wrote.
+        """
+        return None
+
