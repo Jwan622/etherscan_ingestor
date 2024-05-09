b66598a0c9e690517cdc4938cfed90d9a1cdb915
Jeffrey <jwan622@gmail.com>
2024-05-09 14:12:41 -0400

    add db test for session

--- nascent_etherscan_ingestor/tests/unit/test_db.py
+++ nascent_etherscan_ingestor/tests/unit/test_db.py
@@ -15,7 +15,15 @@ def mock_create_all(mocker):
     return mocker.patch.object(Base.metadata, 'create_all')
 
 
-def test_init_db(mock_create_engine, mock_create_all):
+@pytest.fixture
+def mock_session(mocker):
+    mock_session = mocker.Mock()
+    mocker.patch('src.assignment.db.scoped_session', return_value=mock_session)
+
+    return mock_session
+
+
+def test_init_db_creates_engine_and_tables(mock_create_engine, mock_create_all):
     mock_created_engine, mock_create_engine = mock_create_engine
     test_url = 'test_url'
 
@@ -23,3 +31,12 @@ def test_init_db(mock_create_engine, mock_create_all):
 
     mock_create_engine.assert_called_once_with(test_url)
     mock_create_all.assert_called_once_with(mock_created_engine)
+
+
+def test_init_db_returns_session(mock_create_engine, mock_create_all, mock_session):
+    test_url = 'test_url'
+    expected = mock_session
+
+    actual = init_db(test_url)
+
+    assert actual == expected
